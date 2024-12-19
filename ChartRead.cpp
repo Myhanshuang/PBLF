01c4f7ca1c1574f0aa1737ceaa23b60c0dd7afe3
Logib <Logib@qq.com>
2024-12-19 17:26:32 +0800

    W's_merge

---
+++ ChartRead.cpp
@@ -0,0 +1,169 @@
+//
+// Created by Logib on 2024/12/11.
+//
+
+#ifndef _GLIBCXX_CSTDIO
+#include <cstdio>
+#endif //_GLIBCXX_CSTDIO
+
+#ifndef _GLIBCXX_CSTRING
+#include <cstring>
+#endif //_GLIBCXX_CSTRING
+
+/**
+ * @brief get value in file (int)
+ * @param File
+ * @param val
+ */
+void getValueInt(FILE *File, int& val){
+    val = 0;
+    char ch = (char )getc(File);
+    while (ch > '9' || ch < '0') ch = (char )getc(File);
+    while (ch < '9'+1 && ch > '0'-1){
+        val = (val << 3) + (val << 1) + ch - '0';
+        ch = (char )getc(File);
+    }
+}
+
+/**
+ * @brief get value in file (short)
+ * @param File
+ * @param val
+ */
+void getValueShort(FILE *File, short & val){
+    val = 0;
+    char ch = (char )getc(File);
+    while (ch > '9' || ch < '0') ch = (char )getc(File);
+    while (ch < '9'+1 && ch > '0'-1){
+        val = static_cast <short>((val << 3) + (val << 1) + ch - '0');
+        ch = (char )getc(File);
+    }
+}
+
+/**
+ * @brief get value in file (short)
+ * @param File
+ * @param val
+ */
+void getValueLDouble(FILE *File, long double& val){
+    val = 0.0l;
+    char ch = (char )getc(File);
+    while (ch > '9' || ch < '0') ch = (char )getc(File);
+    while (ch < '9'+1 && ch > '0'-1){
+        val = val*10.0l + ch - '0';
+        ch = (char )getc(File);
+    }
+    if (ch != '.') return ;
+    long double p = 0.1;
+    ch = (char )getc(File);
+    while (ch < '9'+1 && ch > '0'-1){
+        val += (ch - '0')*p;
+        p/=10.0l;
+        ch = (char )getc(File);
+    }
+}
+
+/**
+ * @brief use KMP to get the position of the keyword
+ * @param File
+ * @param KeyWord
+ * @return \c bool ( 1 = found )
+ ***/
+bool getKeyWord(FILE *File, const char *KeyWord){
+    char c = '\0';
+    auto Length = static_cast <short>(strlen(KeyWord));
+    auto nxt = new short [Length+1];
+    nxt[0] = -1;
+    for (short i = 0, j = -1; i < Length-1; ){
+        if (j == -1 || KeyWord[i] == KeyWord[j]){
+            ++i;
+            ++j;
+            nxt[i] = j;
+        }
+        else j = nxt[j];
+    }
+    short j = -1;
+    while (!feof(File)){
+        if (j == -1 || c == KeyWord[j]){
+            c = (char )fgetc(File);
+            ++j;
+        }
+        else j = nxt[j];
+        if (j == Length){
+            delete[] nxt;
+            return true;
+        }
+    }
+    delete[] nxt;
+    return false;
+}
+
+
+/**
+ * @brief use multiple KMP to get the position of the keywords
+ * @param File
+ * @param KeyWord1
+ * @param KeyWord2
+ * @return \c short ( 1 = found 1, 2 = found 2, 0 = all not found)
+ ***/
+short getKeyWords(FILE *File, const char *KeyWord1, const char *KeyWord2){
+    char c = '\0';
+    auto Length1 = static_cast <short>(strlen(KeyWord1)), Length2 = static_cast <short>(strlen(KeyWord2));
+    auto nxt1 = new short [Length1+1];
+    auto nxt2 = new short [Length2+1];
+    nxt1[0] = -1, nxt2[0] = -1;
+    for (short i = 0, j = -1; i < Length1-1; ){
+        if (j == -1 || KeyWord1[i] == KeyWord1[j]){
+            ++i;
+            ++j;
+            nxt1[i] = j;
+        }
+        else j = nxt1[j];
+    }
+    for (short i = 0, j = -1; i < Length2-1; ){
+        if (j == -1 || KeyWord2[i] == KeyWord2[j]){
+            ++i;
+            ++j;
+            nxt2[i] = j;
+        }
+        else j = nxt2[j];
+    }
+    short j1 = -1, j2 = -1;
+    bool f1, f2;
+    while (!feof(File)){
+        f1 = (j1 == -1 || c == KeyWord1[j1]);
+        f2 = (j2 == -1 || c == KeyWord2[j2]);
+        if (f1 && f2){
+            c = (char )fgetc(File);
+            ++j1;
+            ++j2;
+        }
+        else {
+            while (!f1){
+                j1 = nxt1[j1];
+                f1 = (j1 == -1 || c == KeyWord1[j1]);
+            }
+            while (!f2){
+                j2 = nxt2[j2];
+                f2 = (j2 == -1 || c == KeyWord2[j2]);
+            }
+        }
+        if (j1 == Length1){
+            delete[] nxt1;
+            delete[] nxt2;
+            return 1;
+        }
+        if (j2 == Length2){
+            delete[] nxt1;
+            delete[] nxt2;
+            return 2;
+        }
+    }
+    delete[] nxt1;
+    delete[] nxt2;
+    return 0;
+}
+
+void putWords(FILE *File, const char *Words){
+    for (short t = 0; Words[t] != '\0'; ++t) putc((int )Words[t], File);
+}
---
+++ User.cpp
@@ -0,0 +1,84 @@
+//
+// Created by Logib on 2024/12/17.
+//
+
+#ifndef KEYRHYTHM_CHART_H
+#include "Chart.h"
+#endif
+
+
+/**
+ * @class User
+ * @brief initialize user data
+ */
+User :: User(){
+    memset(this ->userName, 0, sizeof(char )*13);
+    memset(this ->userName, 0, sizeof(char )*21);
+}
+
+/**
+ * @class User
+ * @brief to save user data
+ * @param Saving (\c FILE )
+ */
+void User :: save(FILE *Saving){
+    if (this ->userName[0] == '\0' || this ->userName[12] != '\0')
+        throw ChartError(7);
+    if (this ->Password[0] == '\0' || this ->Password[20] != '\0')
+        throw ChartError(8);
+    putc((int )'\n', Saving);
+    putWords(Saving, this ->userName);
+    putc((int )'\n', Saving);
+    char *encode = base64_encode(this ->Password);
+    putWords(Saving, encode);
+    putc((int )'\n', Saving);
+    free(encode);
+}
+
+/**
+ * @class User
+ * @brief to check if this is new user
+ * @param Saving (\c FILE )
+ * @return \c bool (1 = new, 0 = old)
+ */
+bool User :: newUser(FILE *Saving) {
+    fseek(Saving, 0, SEEK_SET);
+    return !( getKeyWord(Saving, this ->userName) );
+}
+
+/**
+ * @class User
+ * @brief to check whether the user's password is right
+ * @param Saving (\c FILE )
+ * @return \c bool (1 = right, 0 = wrong)
+ */
+bool User :: correct(FILE *Saving) {
+    fseek(Saving, 0, SEEK_SET);
+    if ( !getKeyWord(Saving, this ->userName) ) throw ChartError(9);
+    char *tmp = new char [257];
+    memset(tmp, 0, sizeof (char )*257);
+    int t = 0;
+    char ch = (char )getc(Saving);
+    while (ch != '=' && !(ch > '0'-1 && ch < '9'+1)
+           && !(ch > 'A'-1 && ch < 'Z'+1) && !(ch > 'a'-1 && ch < 'z'+1))
+        ch = (char )getc(Saving);
+
+    while (ch == '=' || (ch > '0'-1 && ch < '9'+1)
+           || (ch > 'A'-1 && ch < 'Z'+1) || (ch > 'a'-1 && ch < 'z'+1)){
+        tmp[t++] = ch;
+        ch = (char )getc(Saving);
+    }
+    tmp[t]='\0';
+    char *decode = base64_decode(tmp);
+    delete[] tmp;
+    if ( strcmp(decode, this ->Password) != 0){
+        free(decode);
+        return false;
+    }
+    free(decode);
+    return true;
+}
+
+void UserSaveData :: save(FILE *History) {
+
+}
